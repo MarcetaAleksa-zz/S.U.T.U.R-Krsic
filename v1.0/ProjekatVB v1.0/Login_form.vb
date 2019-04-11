9f00926808d86200a4e1fb9bbfd221760d041cff
MarcetaAleksa <30878192+MarcetaAleksa@users.noreply.github.com>
2019-04-11 20:50:56 +0200

    Izmjena v1.61
    Funkcionalan Login, povezan sa Bazom.
    Problemi se javljaju u Bool logici, gdje trebam da rqazvrstam koji nalozi su Admin a koji User.

--- v1.0/ProjekatVB v1.0/Login_form.vb
+++ v1.0/ProjekatVB v1.0/Login_form.vb
@@ -1,4 +1,6 @@
-﻿Public Class Login_form
+﻿Imports System.Data.SqlClient
+
+Public Class Login_form
     Private Sub Username_Form_Box_Enter(sender As Object, e As EventArgs) Handles Username_Form_Box.Enter
         If (Username_Form_Box.Text = "Enter Username here") Then
             Username_Form_Box.Text = ""
@@ -31,20 +33,42 @@
     End Sub
     Private Sub Login_Button_Click(sender As Object, e As EventArgs) Handles Login_Button.Click
 
-        If (Username_Form_Box.Text = "Admin" AndAlso Password_Form_Box.Text = "Password") Then
-            MessageBox.Show("Welcome to Administrator Panel")
+        Dim connection As New SqlConnection("Server = TESTTHENEXT2\SQLEXPRESS; Database = Projekat; Integrated Security = true")
+
+        Dim command As New SqlCommand("SELECT * FROM Projekat.dbo.Login  where Nalog = @Nalog and Lozinka = @Lozinka", connection)
+        Dim Tip(10)
+        command.Parameters.Add("@Nalog", SqlDbType.NChar).Value = Username_Form_Box.Text
+
+        command.Parameters.Add("@Lozinka", SqlDbType.VarChar).Value = Password_Form_Box.Text
+
+
+
+        Dim adapter As New SqlDataAdapter(command)
+
+
+        Dim table As New DataTable()
+        adapter.Fill(table)
+
+        If table.Rows.Count() <= 0 Then
+            MessageBox.Show("Please enter correct Username and Password")
+            Password_Form_Box.PasswordChar = "*"
+        Else
+            ' Dim another_command As New SqlCommand("SELECT * FROM Projekat.dbo.Login  where Tip_Naloga = @Tip_Naloga", connection)
+            'command.Parameters.Add("@Tip_Naloga", SqlDbType.Char).Value = Tip
             Me.Hide()
             Administrator.Show()
-            Password_Form_Box.Text = ""
-        ElseIf (Username_Form_Box.Text = "User" AndAlso Password_Form_Box.Text = "User") Then
-            MessageBox.Show("Welcome to User Panel")
-            Me.Hide()
-            User.Show()
-            Password_Form_Box.Text = ""
-        Else
-            MessageBox.Show("Please enter correct Username and Password ")
-            Password_Form_Box.PasswordChar = ""
-            Password_Form_Box.Text = "Enter Password here"
+
+            'If (Tip("@Nalog") = False) Then
+            ' MessageBox.Show("Welcome to User Panel")
+            'Me.Hide()
+            'User.Show()
+            'Password_Form_Box.Text = ""
+            ' ElseIf (Tip("@Nalog") = True) Then
+            'MessageBox.Show("Welcome to Administrator Panel")
+            'Me.Hide()
+            'Administrator.Show()
+            'Password_Form_Box.Text = ""
+            'End If
         End If
     End Sub
 
