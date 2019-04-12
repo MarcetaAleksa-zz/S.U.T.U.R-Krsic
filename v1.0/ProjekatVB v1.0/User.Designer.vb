2a24a3f4f94512ac083559af9feeb9bfad20195d
MarcetaAleksa <30878192+MarcetaAleksa@users.noreply.github.com>
2019-04-12 12:09:02 +0200

    Izmjene v1.7
    Male graficke ispravke na User Panelu.

--- v1.0/ProjekatVB v1.0/User.Designer.vb
+++ v1.0/ProjekatVB v1.0/User.Designer.vb
@@ -22,17 +22,44 @@ Partial Class User
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(User))
+        Me.Back_Button = New System.Windows.Forms.Button()
+        Me.Exit_Button = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'Back_Button
+        '
+        Me.Back_Button.Image = CType(resources.GetObject("Back_Button.Image"), System.Drawing.Image)
+        Me.Back_Button.Location = New System.Drawing.Point(12, 12)
+        Me.Back_Button.Name = "Back_Button"
+        Me.Back_Button.Size = New System.Drawing.Size(39, 35)
+        Me.Back_Button.TabIndex = 3
+        Me.Back_Button.UseVisualStyleBackColor = True
+        '
+        'Exit_Button
+        '
+        Me.Exit_Button.Location = New System.Drawing.Point(57, 12)
+        Me.Exit_Button.Name = "Exit_Button"
+        Me.Exit_Button.Size = New System.Drawing.Size(126, 35)
+        Me.Exit_Button.TabIndex = 7
+        Me.Exit_Button.Text = "Exit"
+        Me.Exit_Button.UseVisualStyleBackColor = True
         '
         'User
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Exit_Button)
+        Me.Controls.Add(Me.Back_Button)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "User"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "User Panel"
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents Back_Button As Button
+    Friend WithEvents Exit_Button As Button
 End Class
--- v1.0/ProjekatVB v1.0/User.vb
+++ v1.0/ProjekatVB v1.0/User.vb
@@ -1,3 +1,10 @@
 ﻿Public Class User
+    Private Sub Back_Button_Click(sender As Object, e As EventArgs) Handles Back_Button.Click
+        Me.Hide()
+        Login_form.Show()
+    End Sub
 
+    Private Sub Exit_Button_Click(sender As Object, e As EventArgs) Handles Exit_Button.Click
+        Me.Close()
+    End Sub
 End Class
