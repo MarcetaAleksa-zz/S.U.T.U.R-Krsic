a0dc9aebb9d55dda195c21f195fe062dcbd97f57
Panzo98 <49492024+Panzo98@users.noreply.github.com>
2019-05-28 00:34:40 +0200

    Kao katalog probavam, jebe me skrol u tabpage, tu ubacim panel i nan jemu treba autoscrool biti  true al nesto ence pa se jebendisem al cu napraviti odo sad kuci pero hoce spavati

--- MainProgram/ProjekatVB v1.0/Display_Goods_Guest.Designer.vb
+++ MainProgram/ProjekatVB v1.0/Display_Goods_Guest.Designer.vb
@@ -23,90 +23,34 @@ Partial Class Display_Goods_Guest
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Display_Goods_Guest))
-        Me.TabControl1 = New System.Windows.Forms.TabControl()
-        Me.TabSanduci = New System.Windows.Forms.TabPage()
-        Me.TabKrstovi = New System.Windows.Forms.TabPage()
-        Me.TabCvijece = New System.Windows.Forms.TabPage()
-        Me.TabSvijece = New System.Windows.Forms.TabPage()
-        Me.TabHladnjaca = New System.Windows.Forms.TabPage()
-        Me.TabPrevoz = New System.Windows.Forms.TabPage()
         Me.Back_Button = New System.Windows.Forms.Button()
         Me.Exit_Button = New System.Windows.Forms.Button()
         Me.Panel3 = New System.Windows.Forms.Panel()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.TabControl1.SuspendLayout()
+        Me.TabPrevoz = New System.Windows.Forms.TabPage()
+        Me.TabHladnjaca = New System.Windows.Forms.TabPage()
+        Me.TabSvijece = New System.Windows.Forms.TabPage()
+        Me.TabCvijece = New System.Windows.Forms.TabPage()
+        Me.TabKrstovi = New System.Windows.Forms.TabPage()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
         Me.Panel3.SuspendLayout()
+        Me.TabKrstovi.SuspendLayout()
+        Me.TabControl1.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'TabControl1
-        '
-        Me.TabControl1.Controls.Add(Me.TabSanduci)
-        Me.TabControl1.Controls.Add(Me.TabKrstovi)
-        Me.TabControl1.Controls.Add(Me.TabCvijece)
-        Me.TabControl1.Controls.Add(Me.TabSvijece)
-        Me.TabControl1.Controls.Add(Me.TabHladnjaca)
-        Me.TabControl1.Controls.Add(Me.TabPrevoz)
-        Me.TabControl1.Location = New System.Drawing.Point(12, 117)
-        Me.TabControl1.Name = "TabControl1"
-        Me.TabControl1.SelectedIndex = 0
-        Me.TabControl1.Size = New System.Drawing.Size(776, 426)
-        Me.TabControl1.TabIndex = 0
-        '
-        'TabSanduci
-        '
-        Me.TabSanduci.Location = New System.Drawing.Point(4, 22)
-        Me.TabSanduci.Name = "TabSanduci"
-        Me.TabSanduci.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabSanduci.Size = New System.Drawing.Size(768, 400)
-        Me.TabSanduci.TabIndex = 0
-        Me.TabSanduci.Text = "Sanduci"
-        Me.TabSanduci.UseVisualStyleBackColor = True
-        '
-        'TabKrstovi
-        '
-        Me.TabKrstovi.Location = New System.Drawing.Point(4, 22)
-        Me.TabKrstovi.Name = "TabKrstovi"
-        Me.TabKrstovi.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabKrstovi.Size = New System.Drawing.Size(768, 400)
-        Me.TabKrstovi.TabIndex = 1
-        Me.TabKrstovi.Text = "Krstovi"
-        Me.TabKrstovi.UseVisualStyleBackColor = True
-        '
-        'TabCvijece
-        '
-        Me.TabCvijece.Location = New System.Drawing.Point(4, 22)
-        Me.TabCvijece.Name = "TabCvijece"
-        Me.TabCvijece.Size = New System.Drawing.Size(768, 400)
-        Me.TabCvijece.TabIndex = 2
-        Me.TabCvijece.Text = "Cvijece"
-        Me.TabCvijece.UseVisualStyleBackColor = True
-        '
-        'TabSvijece
-        '
-        Me.TabSvijece.Location = New System.Drawing.Point(4, 22)
-        Me.TabSvijece.Name = "TabSvijece"
-        Me.TabSvijece.Size = New System.Drawing.Size(768, 400)
-        Me.TabSvijece.TabIndex = 3
-        Me.TabSvijece.Text = "Svijece"
-        Me.TabSvijece.UseVisualStyleBackColor = True
-        '
-        'TabHladnjaca
-        '
-        Me.TabHladnjaca.Location = New System.Drawing.Point(4, 22)
-        Me.TabHladnjaca.Name = "TabHladnjaca"
-        Me.TabHladnjaca.Size = New System.Drawing.Size(768, 400)
-        Me.TabHladnjaca.TabIndex = 4
-        Me.TabHladnjaca.Text = "Hladnjace"
-        Me.TabHladnjaca.UseVisualStyleBackColor = True
-        '
-        'TabPrevoz
-        '
-        Me.TabPrevoz.Location = New System.Drawing.Point(4, 22)
-        Me.TabPrevoz.Name = "TabPrevoz"
-        Me.TabPrevoz.Size = New System.Drawing.Size(768, 400)
-        Me.TabPrevoz.TabIndex = 5
-        Me.TabPrevoz.Text = "Prevoz"
-        Me.TabPrevoz.UseVisualStyleBackColor = True
         '
         'Back_Button
         '
@@ -146,11 +90,159 @@ Partial Class Display_Goods_Guest
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Artikli"
         '
+        'TabPrevoz
+        '
+        Me.TabPrevoz.Location = New System.Drawing.Point(4, 22)
+        Me.TabPrevoz.Name = "TabPrevoz"
+        Me.TabPrevoz.Size = New System.Drawing.Size(774, 409)
+        Me.TabPrevoz.TabIndex = 5
+        Me.TabPrevoz.Text = "Prevoz"
+        Me.TabPrevoz.UseVisualStyleBackColor = True
+        '
+        'TabHladnjaca
+        '
+        Me.TabHladnjaca.Location = New System.Drawing.Point(4, 22)
+        Me.TabHladnjaca.Name = "TabHladnjaca"
+        Me.TabHladnjaca.Size = New System.Drawing.Size(774, 409)
+        Me.TabHladnjaca.TabIndex = 4
+        Me.TabHladnjaca.Text = "Hladnjace"
+        Me.TabHladnjaca.UseVisualStyleBackColor = True
+        '
+        'TabSvijece
+        '
+        Me.TabSvijece.Location = New System.Drawing.Point(4, 22)
+        Me.TabSvijece.Name = "TabSvijece"
+        Me.TabSvijece.Size = New System.Drawing.Size(774, 409)
+        Me.TabSvijece.TabIndex = 3
+        Me.TabSvijece.Text = "Svijece"
+        Me.TabSvijece.UseVisualStyleBackColor = True
+        '
+        'TabCvijece
+        '
+        Me.TabCvijece.Location = New System.Drawing.Point(4, 22)
+        Me.TabCvijece.Name = "TabCvijece"
+        Me.TabCvijece.Size = New System.Drawing.Size(774, 409)
+        Me.TabCvijece.TabIndex = 2
+        Me.TabCvijece.Text = "Cvijece"
+        Me.TabCvijece.UseVisualStyleBackColor = True
+        '
+        'TabKrstovi
+        '
+        Me.TabKrstovi.Controls.Add(Me.Panel1)
+        Me.TabKrstovi.Location = New System.Drawing.Point(4, 22)
+        Me.TabKrstovi.Name = "TabKrstovi"
+        Me.TabKrstovi.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabKrstovi.Size = New System.Drawing.Size(774, 409)
+        Me.TabKrstovi.TabIndex = 1
+        Me.TabKrstovi.Text = "Krstovi"
+        Me.TabKrstovi.UseVisualStyleBackColor = True
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabKrstovi)
+        Me.TabControl1.Controls.Add(Me.TabCvijece)
+        Me.TabControl1.Controls.Add(Me.TabSvijece)
+        Me.TabControl1.Controls.Add(Me.TabHladnjaca)
+        Me.TabControl1.Controls.Add(Me.TabPrevoz)
+        Me.TabControl1.Location = New System.Drawing.Point(12, 117)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(782, 435)
+        Me.TabControl1.TabIndex = 0
+        '
+        'Panel1
+        '
+        Me.Panel1.AutoScroll = True
+        Me.Panel1.Controls.Add(Me.PictureBox4)
+        Me.Panel1.Controls.Add(Me.PictureBox3)
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.PictureBox2)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Location = New System.Drawing.Point(20, 14)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(718, 427)
+        Me.Panel1.TabIndex = 0
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(21, 16)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(88, 81)
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(144, 38)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(139, 25)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Krst hrastov"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(144, 145)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(139, 25)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Krst hrastov"
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Location = New System.Drawing.Point(21, 123)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(88, 81)
+        Me.PictureBox2.TabIndex = 4
+        Me.PictureBox2.TabStop = False
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(144, 247)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(139, 25)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Krst hrastov"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(144, 354)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(139, 25)
+        Me.Label5.TabIndex = 3
+        Me.Label5.Text = "Krst hrastov"
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Location = New System.Drawing.Point(21, 332)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(88, 81)
+        Me.PictureBox3.TabIndex = 4
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Location = New System.Drawing.Point(21, 225)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(88, 81)
+        Me.PictureBox4.TabIndex = 4
+        Me.PictureBox4.TabStop = False
+        '
         'Display_Goods_Guest
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(799, 554)
+        Me.ClientSize = New System.Drawing.Size(806, 569)
         Me.Controls.Add(Me.Panel3)
         Me.Controls.Add(Me.Exit_Button)
         Me.Controls.Add(Me.Back_Button)
@@ -160,22 +252,36 @@ Partial Class Display_Goods_Guest
         Me.Name = "Display_Goods_Guest"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Guest Panel"
-        Me.TabControl1.ResumeLayout(False)
         Me.Panel3.ResumeLayout(False)
         Me.Panel3.PerformLayout()
+        Me.TabKrstovi.ResumeLayout(False)
+        Me.TabControl1.ResumeLayout(False)
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
-
-    Friend WithEvents TabControl1 As TabControl
-    Friend WithEvents TabSanduci As TabPage
-    Friend WithEvents TabKrstovi As TabPage
-    Friend WithEvents TabCvijece As TabPage
-    Friend WithEvents TabSvijece As TabPage
-    Friend WithEvents TabHladnjaca As TabPage
-    Friend WithEvents TabPrevoz As TabPage
     Friend WithEvents Back_Button As Button
     Friend WithEvents Exit_Button As Button
     Friend WithEvents Panel3 As Panel
     Friend WithEvents Label1 As Label
+    Friend WithEvents TabPrevoz As TabPage
+    Friend WithEvents TabHladnjaca As TabPage
+    Friend WithEvents TabSvijece As TabPage
+    Friend WithEvents TabCvijece As TabPage
+    Friend WithEvents TabKrstovi As TabPage
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
 End Class
--- MainProgram/ProjekatVB v1.0/Display_Goods_Guest.vb
+++ MainProgram/ProjekatVB v1.0/Display_Goods_Guest.vb
@@ -6,4 +6,8 @@
     Private Sub Exit_Button_Click(sender As Object, e As EventArgs) Handles Exit_Button.Click
         Me.Close()
     End Sub
+
+    Private Sub Display_Goods_Guest_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TabKrstovi.AutoScroll = True
+    End Sub
 End Class
